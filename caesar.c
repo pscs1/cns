2cc6da2605848f27bb5301b8640dd557c5c16148
pscs1 <42995804+pscs1@users.noreply.github.com>
2023-05-25 10:59:13 +0530

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
-
 int main()
 {
 	char a[50], b, c[50];
-	int i, key, key1;
+	int i, key;
 	printf("Enter a message to encrypt\n");
 	fgets(a, 50, stdin);
 	printf("Enter a key\n");
@@ -12,7 +11,7 @@ int main()
 	for(i=0;a[i]!='\0';i++)
 	{
 		b=a[i];
-		
+	
 		if(b>='a' && b<='z')
 		{
 			b=(b-'a'+key)%26+'a';
@@ -28,14 +27,12 @@ int main()
 		a[i]=b;
 	}
 	printf("Encrypted message: %s\n", a);
-	
 	printf("Enter a message to decrypt\n");
 	getchar();
 	fgets(c, 20, stdin);
 	for(i=0;c[i]!='\0';i++)
 	{
 		b=c[i];
-		
 		if(b>='a' && b<='z')
 		{
 			b=(b-'a'-key)%26+'a';
@@ -50,7 +47,6 @@ int main()
 		}
 		c[i]=b;
 	}
-	
 	printf("Decrypted message: %s\n", c);
 	return 0;
 }
