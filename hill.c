53589b542ab54567f7b3ab82feb0c94941d61890
pscs1 <42995804+pscs1@users.noreply.github.com>
2023-05-25 11:22:51 +0530

    Update hill.c

--- hill.c
+++ hill.c
@@ -1,46 +1,83 @@
-    #include<stdio.h>
-    #include<string.h>
-
-    int main() {
-        unsigned int a[3][3] = { { 6, 24, 1 }, { 13, 16, 10 }, { 20, 17, 15 } };
-        unsigned int b[3][3] = { { 8, 5, 10 }, { 21, 8, 21 }, { 21, 12, 8 } };
-        int i, j;
-        unsigned int c[20], d[20];
-        char msg[20];
-        int determinant = 0, t = 0;
-
-        printf("Enter plain text\n ");
-        scanf("%s", msg);
-      
-        for (i = 0; i < 3; i++) {
-            c[i] = msg[i] - 65;
-            printf("%d ", c[i]);
-        }
-
-        for (i = 0; i < 3; i++) {
-            t = 0;
-            for (j = 0; j < 3; j++) {
-                t = t + (a[i][j] * c[j]);
-            }
-            d[i] = t % 26;
-        }
-
-        printf("\nEncrypted Cipher Text :");
-
-        for (i = 0; i < 3; i++)
-            printf(" %c", d[i] + 65);
-        for (i = 0; i < 3; i++) {
-            t = 0;
-            for (j = 0; j < 3; j++) {
-                t = t + (b[i][j] * d[j]);
-            }
-
-            c[i] = t % 26;
-        }
-        printf("\nDecrypted Cipher Text :");
-
-        for (i = 0; i < 3; i++)
-            printf(" %c", c[i] + 65);
-            printf("\n");
-        return 0;
-    }
+#include<stdio.h>
+#include<math.h>
+ 
+float encrypt[3][1], decrypt[3][1], a[3][3], b[3][3], mes[3][1], c[3][3];
+ 
+void encryption(); 
+void decryption(); 
+void getKeyMessage();
+void inverse(); 
+ 
+void main() {
+getKeyMessage();
+encryption();
+decryption();
+}
+ 
+void encryption() {
+int i, j, k;
+for(i = 0; i < 3; i++)
+for(j = 0; j < 1; j++)
+for(k = 0; k < 3; k++)
+encrypt[i][j] = encrypt[i][j] + a[i][k] * mes[k][j];
+printf("\nEncrypted string is: ");
+for(i = 0; i < 3; i++)
+printf("%c", (char)(fmod(encrypt[i][0], 26) + 97));
+ 
+}
+ 
+void decryption() {
+int i, j, k;
+inverse();
+for(i = 0; i < 3; i++)
+for(j = 0; j < 1; j++)
+for(k = 0; k < 3; k++)
+decrypt[i][j] = decrypt[i][j] + b[i][k] * encrypt[k][j];
+printf("\nDecrypted string is: ");
+for(i = 0; i < 3; i++)
+printf("%c", (char)(fmod(decrypt[i][0], 26) + 97));
+printf("\n");
+}
+ 
+void getKeyMessage() {
+int i, j;
+char msg[3];
+ 
+printf("Enter 3x3 matrix for key (It should be inversible):\n");
+for(i = 0; i < 3; i++)
+for(j = 0; j < 3; j++) {
+scanf("%f", &a[i][j]);
+c[i][j] = a[i][j];
+}
+printf("\nEnter a 3 letter string: ");
+scanf("%s", msg);
+for(i = 0; i < 3; i++)
+mes[i][0] = msg[i] - 97;
+}
+ 
+void inverse() {
+int i, j, k;
+float p, q;
+for(i = 0; i < 3; i++)
+for(j = 0; j < 3; j++) {
+if(i == j)
+b[i][j]=1;
+else
+b[i][j]=0;
+}
+for(k = 0; k < 3; k++) {
+for(i = 0; i < 3; i++) {
+p = c[i][k];
+q = c[k][k];
+for(j = 0; j < 3; j++) {
+if(i != k) {
+c[i][j] = c[i][j]*q - p*c[k][j];
+b[i][j] = b[i][j]*q - p*b[k][j];
+}
+}
+}
+}
+for(i = 0; i < 3; i++)
+for(j = 0; j < 3; j++)
+b[i][j] = b[i][j] / c[i][i];
+}
