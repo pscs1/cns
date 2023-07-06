732ae128f65638f237cebde59e1c47c0a8245846
pscs1 <42995804+pscs1@users.noreply.github.com>
2023-07-07 00:32:35 +0530

    Update dh.c

--- dh.c
+++ dh.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
-int compute(int a, int m, int n)
+int power_mod(int b, int e, int m)
 {
-    int r;
-    int y = 1;
-    while (m > 0)
+    int r=1;
+    while(e)
     {
-        r = m % 2;
-        if (r == 1) {
-            y = (y*a) % n;
+        if (e&1) {
+            r = (r*b) % m;
         }
-        a = a*a % n;
-        m = m / 2;
+        b = (b*b) % m;
+        e>>=1;
     }
-    return y;
+    return r;
 }
- 
 int main()
 {
-    int p = 23;
-    int g = 9;
-    int a, b;    
-    int A, B;
-   printf("Enter your private key(XA):\n");
-   scanf("%d",&a);
-   A = compute(g, a, p);
-   printf("Enter your friend's private key(XB):\n");
-   scanf("%d",&b);
-   B = compute(g, b, p);
-    int keyA = compute(B, a, p);
-    int keyB = compute(A, b, p);
+    int q, alpha, Xa, Xb, Ya, Yb;
+    printf("Enter a prime no.:\n");
+    scanf("%d",&q);
+    printf("Enter a primitive root of %d\n", q);
+    scanf("%d",&alpha);
+    printf("Enter your private key(XA):\n");
+    scanf("%d",&Xa);
+    Ya = power_mod(alpha, Xa, q);
+    printf("Enter your friend's private key(XB):\n");
+    scanf("%d",&Xb);
+    Yb = power_mod(alpha, Xb, q);
+    int keyA = power_mod(Yb, Xa, q);
+    int keyB = power_mod(Ya, Xb, q);
     printf("Your secret key is %d\nYour friend's secret key is %d\n", keyA, keyB);
     return 0;
 }
